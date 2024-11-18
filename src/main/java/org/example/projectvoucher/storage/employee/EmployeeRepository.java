412a76f436d53ead6773c50089dd9752d9b82cde
voynpower <hh4868047@gmail.com>
2024-11-18 17:18:14 +0500

    영속화 로직 storage 레이어로 이동

---
+++ src/main/java/org/example/projectvoucher/storage/employee/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package org.example.projectvoucher.storage.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+}
---
+++ src/test/java/org/example/projectvoucher/EmployeeServiceTest.java
@@ -0,0 +1,39 @@
+package org.example.projectvoucher;
+
+import org.example.projectvoucher.app.controller.response.EmployeeResponse;
+import org.example.projectvoucher.domain.employee.EmployeeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class EmployeeServiceTest {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @DisplayName("회원 생성후 조회가 가능하다.")
+    @Test
+    public void test1() {
+        // given
+        String name = "홍길동";
+        String position = "Junior";
+        String department = "SpringDeveloper";
+
+        // when
+        Long no = employeeService.create(name, position, department);
+        EmployeeResponse employeeResponse = employeeService.get(no);
+
+        // then
+        assertThat(employeeResponse).isNotNull();
+        assertThat(employeeResponse.no()).isEqualTo(no);
+        assertThat(employeeResponse.name()).isEqualTo(name);
+        assertThat(employeeResponse.position()).isEqualTo(position);
+        assertThat(employeeResponse.department()).isEqualTo(department);
+        System.out.println(employeeResponse);
+
+
+    }
+}
